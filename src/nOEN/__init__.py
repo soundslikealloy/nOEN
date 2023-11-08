8145a8d37d11b80617dc851e91f5ab6f809b744a
soundslikealloy <eloi.mrp@gmail.com>
2023-11-08 10:42:58 -0600

    Update __init__.py

--- src/nOEN/__init__.py
+++ src/nOEN/__init__.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 27 10:04:00 2023
+# Copyright 2023 by Eloi Martinez-Rabert.  All rights reserved.
+# This code is part of the Python-dna distribution and governed by its
+# license.  Please see the LICENSE.txt file that should have been included
+# as part of this package.
+# doctest: +NORMALIZE_WHITESPACE
+# doctest: +SKIP
 
-@author: 2424069M
+"""
+Lorem ipsum...
+
 """
 
