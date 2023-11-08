c03aef03f2b7216ad5e830f05e75e4d7d98a7085
soundslikealloy <eloi.mrp@gmail.com>
2023-11-08 10:43:21 -0600

    Create stats.py

---
+++ src/nOEN/stats.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 by Eloi Martinez-Rabert.  All rights reserved.
+# This code is part of the Python-dna distribution and governed by its
+# license.  Please see the LICENSE.txt file that should have been included
+# as part of this package.
+# doctest: +NORMALIZE_WHITESPACE
+# doctest: +SKIP
+
+"""
+This module contain functions to perform multivariate rank correlation coefficient.
+
+- :func:`multivarcorr` for the calculus of Tau-N coefficients
+
+- :func:`nOEN` for n-Order Ecological Network analysis (nOEN)
+
+"""
+
