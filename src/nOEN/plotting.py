ba0217d16649ec48bdead5503d0fa900fc937585
soundslikealloy <eloi.mrp@gmail.com>
2023-11-08 10:43:02 -0600

    Create plotting.py

---
+++ src/nOEN/plotting.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 by Eloi Martinez-Rabert.  All rights reserved.
+# This code is part of the Python-dna distribution and governed by its
+# license.  Please see the LICENSE.txt file that should have been included
+# as part of this package.
+# doctest: +NORMALIZE_WHITESPACE
+# doctest: +SKIP
+
+"""
+This module contain functions for plotting the outcomes from nOEN analysis.
+
+- :func:`squarePlot` for ...
+
+- :func:`concentricPlot` for ...
+
+- :func:`getNetwork` for ...
+
+"""
+
